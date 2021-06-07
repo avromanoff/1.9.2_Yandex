b6cceb23bd3f4df8a4bc5d26afb17e70120dd08d
Alexey Romanoff <romanoff.alexey@gmail.com>
2021-06-07 17:03:08 +0300

    true commit

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ from pprint import pprint
 #     Токен можно получить кликнув на полигоне на кнопку "Получить OAuth-токен".
 #
 # HOST: https://cloud-api.yandex.net:443
-# Token: AQAAAAAAjy47AADLW5Zh73ErEEgrokZtbf6PU8c
-# ID: 86c3ae93dfe14892ac93fbfa36e5f830
-# Пароль: 7f93167989fc46bd94f0e94241b814fc
-token = 'AQAAAAAAjy47AADLW5Zh73ErEEgrokZtbf6PU8c'
+# Token: my-token
+# ID: my-ID
+# Пароль: my-password
+token = 'my-token'
 
 class YaUploader:
     def __init__(self, token):
@@ -49,7 +49,7 @@ class YaUploader:
 
 
 if __name__ == '__main__':
-    uploader = YaUploader('AQAAAAAAjy47AADLW5Zh73ErEEgrokZtbf6PU8c')
+    uploader = YaUploader('my-token')
     # result = uploader.upload("c:\my_folder\file.txt")
     # pprint(YaUploader.upload(r"c:\my_folder\file.txt", "file.txt"))
     print(uploader.upload('file.txt', r"c:\my_folder\file.txt"))
